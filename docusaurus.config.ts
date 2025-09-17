82c62a3908d3034de481dc4a62ab10e7cb7f7555
OdinHoang03 <khaihq@gfigroup.io>
2025-09-17 07:19:52 +0000

    Update Docusaurus configuration for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,16 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://innovate-lab.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/thucchien.ai-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Innovate-Lab', // Usually your GitHub org/user name.
   projectName: 'thucchien.ai-docs', // Usually your repo name.
 
+  trailingSlash: false, // Remove trailing slash from URLs
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
