82ed282cdd26ea350c4b377cc23c1e00bdae0ab4
OdinHoang03 <khaihq@gfigroup.io>
2025-09-22 04:10:14 +0000

    Refactor code structure for improved readability and maintainability

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://innovate-lab.github.io',
+  url: 'https://docs.lab.vbi-server.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/thucchien.ai-docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -73,7 +73,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/thucchien-social-card.png',
     navbar: {
       title: 'AI Thực Chiến',
       logo: {
